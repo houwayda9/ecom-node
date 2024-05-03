50748853138dae217f9f402ba53c9110d75b41fa
houwayda9 <121359661+houwayda9@users.noreply.github.com>
2024-05-03 21:37:54 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,8 +5,9 @@ BRANCH=$(echo $GITHUB_REF | sed 's|refs/heads/||')
 PREFIX=$(echo $BRANCH | awk '{print toupper($0)}')_
 
 # Export all environment-specific secrets dynamically
+> env  # Clear or create the env file
 for var in $(env | grep "^$PREFIX"); do
     clean_var_name=$(echo $var | cut -d= -f1 | sed "s/$PREFIX//")
     value=$(echo $var | cut -d= -f2-)
-    echo "$clean_var_name=$value" >> .env
+    echo "$clean_var_name=$value" >> env
 done
