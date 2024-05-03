3680f65be598119dc3c4ebc8f9cbcb58f7c64e1b
houwayda9 <121359661+houwayda9@users.noreply.github.com>
2024-05-03 14:41:30 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,38 +1,11 @@
 #!/bin/bash
 
-set -x  # Activer le mode débogage
+# Extract branch name from GITHUB_REF
+BRANCH=$(echo $GITHUB_REF | sed 's|refs/heads/||')
+PREFIX=$(echo $BRANCH | awk '{print toupper($0)}')_
 
-current_branch=$(git rev-parse --abbrev-ref HEAD)
-echo "Branche actuelle : $current_branch"
-
-# Déterminer l'environnement à partir du nom de la branche
-if [[ $current_branch == "dev" ]]; then
-  environment="development"
-elif [[ $current_branch == "staging" ]]; then
-  environment="staging"
-elif [[ $current_branch == "prod" ]]; then
-  environment="prod"
-else
-  echo "Branche non prise en charge. Sortie."
-  exit 1
-fi
-
-echo "Environnement : $environment"
-
-# Convertir le préfixe d'environnement en majuscules
-prefix=$(echo "$environment" | tr '[:lower:]' '[:upper:]')_
-
-# Créer le fichier .env et injecter les variables et secrets
-env_file=".env.$environment"
-> $env_file  # Créer ou vider le fichier
-
-# Parcourir les variables d'environnement et les secrets avec le préfixe spécifié
-while IFS= read -r var; do
-    clean_var_name=$(echo "$var" | sed "s/^$prefix//")
-    value=$(env | grep "^$var=" | sed 's/^[^=]*=//')
-    echo "$clean_var_name=$value" >> $env_file
-done < <(env | grep "^$prefix" | sed 's/=.*//')
-
-# Afficher le contenu final du fichier .env.$environment
-echo "Contenu du fichier $env_file :"
-cat $env_file
+# Export all environment-specific secrets dynamically
+for var in $(env | grep "^$PREFIX" | sed 's/=.*//'); do
+    clean_var_name=$(echo $var | sed "s/$PREFIX//")
+    echo "$clean_var_name=$(env | grep "^$var=" | sed 's/^[^=]*=//')" >> .env
+done
