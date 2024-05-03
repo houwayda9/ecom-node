1f052efa96efc69d4f072bc3d3959f3d11704853
houwayda9 <121359661+houwayda9@users.noreply.github.com>
2024-05-03 19:18:40 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-# Check the branch name to determine the environment and convert it to uppercase
-environment=$(echo "$GITHUB_REF" | awk -F'/' '{print toupper($NF)}')
+echo "Generating environment file..."
+
+# Determine the environment based on the branch name
+environment=$(echo "$GITHUB_REF_NAME" | tr '[:lower:]' '[:upper:]')
 
 echo "Environment: $environment"
 
+# Create an environment file
+echo "# Environment: $environment" > .env
+
 # List all variables specific to the environment
-# Assuming you have variables named like "PROD_VARIABLE_NAME"
 prefix="${environment}_"
-for var in $(compgen -v | grep "^$prefix"); do
+for var in $(compgen -e | grep "^$prefix"); do
     # Remove the prefix from the variable name
     var_name="${var#$prefix}"
     # Get the value of the variable
     var_value="${!var}"
-    echo "Found variable: $var_name=$var_value"
+    # Write variable to .env file
+    echo "$var_name=$var_value" >> .env
 done
+
+echo "Environment file generated."
