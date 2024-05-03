dc7f64a5c6c86c1c122c292b7ba96c4dfaaa9aa0
houwayda9 <121359661+houwayda9@users.noreply.github.com>
2024-05-03 11:19:47 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -23,3 +23,15 @@ echo "Environment: $environment"
 prefix=$(echo "$environment" | tr '[:lower:]' '[:upper:]')_
 
 echo " $prefix "
+
+
+for var in $(printenv | grep "^$prefix" | cut -d= -f1); do
+    clean_var_name=$(echo $var | sed "s/$prefix//")
+    # If the variable is a secret, access it using GitHub Actions syntax
+    if [[ $var =~ ^$prefix ]]; then
+        value=$(echo "${{ secrets.${var#"$prefix"} }}")
+    else
+        value=$(printenv | grep "^$var=" | sed 's/^[^=]*=//')
+    fi
+    # Append the variable and its value to the .env file
+    echo "$clean_var_name=$value" >> .env
