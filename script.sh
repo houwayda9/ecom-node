7d71872c0959298ae5bf0e112e848d1ef39b6063
houwayda9 <121359661+houwayda9@users.noreply.github.com>
2024-05-03 18:56:16 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,17 +2,8 @@
 
 echo "Generating environment file..."
 
-# Check the branch name to determine the environment
-if [ "$GITHUB_REF" == "refs/heads/prod" ]; then
-    environment="prod"
-elif [ "$GITHUB_REF" == "refs/heads/dev" ]; then
-    environment="dev"
-elif [ "$GITHUB_REF" == "refs/heads/staging" ]; then
-    environment="staging"
-else
-    echo "Unknown environment. Exiting..."
-    exit 1
-fi
+# Check the branch name to determine the environment and convert it to uppercase
+environment=$(echo "$GITHUB_REF" | awk -F'/' '{print toupper($NF)}')
 
 echo "Environment: $environment"
 
@@ -21,7 +12,7 @@ echo "# Environment: $environment" > .env
 
 # List all variables specific to the environment
 # Assuming you have variables named like "PROD_VARIABLE_NAME", "DEV_VARIABLE_NAME", "STAGING_VARIABLE_NAME"
-prefix="${environment^^}_"
+prefix="${environment}_"
 for var in $(compgen -v | grep "^$prefix"); do
     # Remove the prefix from the variable name
     var_name="${var#$prefix}"
