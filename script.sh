fa00380a00d1c29831a4da14aefdb9e3e85984fc
houwayda9 <121359661+houwayda9@users.noreply.github.com>
2024-05-03 10:49:06 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -34,7 +34,7 @@ if [ ! -f "$environment_file" ]; then
 
   # Write filtered environment variables to environment file
   echo "$filtered_variables" > "$environment_file"
-
+  cat $environment_file
   echo "Environment file created: $environment_file"
 fi
 
