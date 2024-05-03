f89c7a4d7b82268df2e67e448f349a69231d1024
houwayda9 <121359661+houwayda9@users.noreply.github.com>
2024-05-03 11:27:46 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -25,7 +25,7 @@ prefix=$(echo "$environment" | tr '[:lower:]' '[:upper:]')_
 echo " $prefix "
 
 
-for var in $(printenv | grep "^$prefix" | cut -d= -f1); do
+for var in $(printenv | grep "^$prefix" ); do
     clean_var_name=$(echo $var | sed "s/$prefix//")
     # If the variable is a secret, access it using GitHub Actions syntax
     if [[ $var =~ ^$prefix ]]; then
