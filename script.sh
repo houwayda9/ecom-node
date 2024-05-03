bb9a6db715f3b9e5bf1dc868ce87a8c52ee1af1e
houwayda9 <121359661+houwayda9@users.noreply.github.com>
2024-05-03 21:45:40 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,16 +4,15 @@
 BRANCH=$(echo $GITHUB_REF | sed 's|refs/heads/||')
 PREFIX=$(echo $BRANCH | awk '{print toupper($0)}')_
 
-# Loop through secrets
-          echo "Secrets:"
-          for secret_name in $(env | grep "^$PREFIX" | sed 's/=.*//'); do
-            secret_value=$(echo "${{ secrets.$secret_name }}")
-            echo "$secret_name=$secret_value"
-          done
+echo "Secrets:"
+for secret_name in $(env | grep "^$PREFIX" | sed 's/=.*//'); do
+    secret_value=${{ secrets.${secret_name#"$PREFIX"} }}
+    echo "$secret_name=$secret_value"
+done
 
-          # Loop through environment variables
-          echo "Variables:"
-          for var_name in $(env | grep "^$PREFIX" | sed 's/=.*//'); do
-            var_value=$(echo "${{ env.$var_name }}")
-            echo "$var_name=$var_value"
-          done
+# Loop through environment variables
+echo "Variables:"
+for var_name in $(env | grep "^$PREFIX" | sed 's/=.*//'); do
+    var_value=${{ env.$var_name }}
+    echo "$var_name=$var_value"
+done
