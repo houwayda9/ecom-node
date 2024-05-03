7934e93ae896a9ab9a4dc6737f9a05c369ee62d0
houwayda9 <121359661+houwayda9@users.noreply.github.com>
2024-05-03 20:52:10 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -11,10 +11,16 @@ echo "Environment: $environment"
 echo "# Environment: $environment" > .env
 
 # List all variables specific to the environment
-prefix="${environment}_"
-for var in $(env | grep "^$PREFIX" | sed 's/=.*//'); do
-    clean_var_name=$(echo $var | sed "s/$PREFIX//")
-    echo "$clean_var_name=$(env | grep "^$var=" | sed 's/^[^=]*=//')" >> .env
+for var in $(compgen -e); do
+    # Check if the variable starts with the environment prefix
+    if [[ "$var" == "${environment}_"* ]]; then
+        # Remove the environment prefix
+        var_name="${var#${environment}_}"
+        # Get the value of the variable
+        var_value="${!var}"
+        # Write variable to .env file
+        echo "$var_name=$var_value" >> .env
+    fi
 done
 
 echo "Environment file generated."
