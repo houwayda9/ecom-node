e2cf87d365e19f9810dccebfd5e3d293122ca409
houwayda9 <121359661+houwayda9@users.noreply.github.com>
2024-05-04 22:28:35 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,21 @@
-# list_variables.sh
-# Create a header for the .env file
-echo "# Exported Environment Variables" > ".env"
+#!/bin/bash
 
-# Filter environment variables by prefix using `grep`
-printenv | grep -E '^(PROD_|DOCKER_)' | while IFS='=' read -r key value; do
-    echo "$key=$value" >> ".env"
+# Extract the branch name from GITHUB_REF
+BRANCH=$(echo "$GITHUB_REF" | sed 's|refs/heads/||')
+# Convert the branch name to uppercase and append an underscore
+PREFIX=$(echo "$BRANCH" | awk '{print toupper($0)}')_
+
+# Create or overwrite the .env file with a header
+echo "# Exported environment variables for ${BRANCH}" > .env
+
+# Loop through all environment variables that start with the PREFIX
+for var in $(env | grep "^$PREFIX" | sed 's/=.*//'); do
+    # Strip the prefix to get the original variable name
+    clean_var_name=$(echo "$var" | sed "s/^$PREFIX//")
+    # Retrieve the value of the environment variable
+    value=$(printenv "$var")
+    # Write the key-value pair to the .env file
+    echo "${clean_var_name}=${value}" >> .env
 done
 
-echo "Selected environment variables exported to .env"
+echo "Environment variables exported to .env"
