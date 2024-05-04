45fded3f83757244c404c780decfd135d69055b6
houwayda9 <121359661+houwayda9@users.noreply.github.com>
2024-05-04 21:37:25 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,15 @@
+#!/bin/bash
 
-echo "Creating .env file..."
-{
-    echo "# Exported Environment Variables"
-    printenv | grep -v "GITHUB_" | grep -v "RUNNER_" | while IFS= read -r line; do
-        key=$(echo "$line" | cut -d= -f1)
-        value=$(echo "$line" | cut -d= -f2-)
-        echo "$key=$value"
-    done
-} > .env
+# Specify the .env file path
+ENV_FILE=".env"
 
-echo "Completed creating .env file."
+# Clear existing file and start fresh
+echo "# Exported Environment Variables" > "$ENV_FILE"
+
+# Loop through all environment variables
+# Customize the grep command to filter only variables with specific prefixes
+printenv | grep -E "^(PROD_)" | while IFS='=' read -r key value; do
+    echo "${key}=${value}" >> "$ENV_FILE"
+done
+
+echo "All environment variables with specified prefixes have been exported to ${ENV_FILE}"
