55ab55463b4129adf4800e71cd44b1f63ffe8b47
houwayda9 <121359661+houwayda9@users.noreply.github.com>
2024-05-08 21:48:47 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
-
-
+# Assuming GITHUB_REF is something like refs/heads/feature-branch
 branch_name=$(echo "$GITHUB_REF" | awk -F'/' '{print tolower($3)}')
 prefix=$(echo "$branch_name" | tr '[:lower:]' '[:upper:]')
 env_file=".env"
+secrets_file=".secrets"
 
+# Define a pattern or list of secret keys
+secrets_pattern="SECRET|PASSWORD|API_KEY"
 
-
-
+# Read environment variables and separate them based on the pattern
 printenv | grep -iE "^${prefix}_" | while IFS='=' read -r key value; do
     stripped_key="${key#${prefix}_}"
-    echo "${stripped_key}=${value}" >> "$env_file"
+    # Check if the key matches the secrets pattern
+    if [[ "$stripped_key" =~ $secrets_pattern ]]; then
+        echo "${stripped_key}=${value}" >> "$secrets_file"
+    else
+        echo "${stripped_key}=${value}" >> "$env_file"
+    fi
 done
 
-# Display the generated .env file for verification
+# Display the generated files for verification
 echo "Generated .env file:"
 cat "$env_file"
 
-
+echo "Generated .secrets file:"
+cat "$secrets_file"
