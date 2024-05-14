f2546b8f83730f646dfd7ce13756b76a9b2b6cee
houwayda9 <121359661+houwayda9@users.noreply.github.com>
2024-05-14 09:35:16 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,16 +5,17 @@ branch_name=$(echo "$GITHUB_REF" | awk -F'/' '{print tolower($3)}')
 prefix=$(echo "$branch_name" | tr '[:lower:]' '[:upper:]')
 env_file=".env_${branch_name}"
 secrets_file=".secrets_${branch_name}"
-
+secrets_pattern="SECRET|PASSWORD|API_KEY|MDP"
+	
 
 # Read environment variables and separate them based on the pattern
 printenv | grep -iE "^${prefix}_" | while IFS='=' read -r key value; do
     stripped_key="${key#${prefix}_}"
     # Check if the key matches the secrets pattern
     if [[ "$stripped_key" =~ $secrets_pattern ]]; then
-        echo "${stripped_key}=${value}" >> "$env_file"
-    else
         echo "${stripped_key}=${value}" >> "$secrets_file"
+    else
+        echo "${stripped_key}=${value}" >> "$env_file"
     fi
 done
 
